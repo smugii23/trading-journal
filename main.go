9157c54bbdd9dba78db69891bda94fb16ac2eee4
smugii23 <smugii0203@gmail.com>
2025-03-08 16:16:07 -0800

    updated main.go

--- main.go
+++ main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"github.com/gofiber/fiber/v2"
 	"trading-journal/config"
 	"trading-journal/routes"
+
+	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	app := fiber.New()
+	// setup database
+	config.ConnectDB()
+	// register HTTP routes
+	routes.SetupRoutes(app)
 }
